7029a981fdf9d08eda2d9a7639ae8c5f5532555f
GotohFumiaki <fgotoh@smaile-lab.com>
2019-07-19 15:16:17 +0900

    1st commit

---
+++ Assets/Scripts/NAudioBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using NAudio.Wave;
+
+public class NAudioBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioFileReader reader = new AudioFileReader("Assets/Audio/Sample.wav");
+        WaveOut waveOut = new WaveOut();
+        waveOut.Init(reader);
+        waveOut.Play();
+
+        // 音楽の再生
+        while(waveOut.PlaybackState == PlaybackState.Playing);
+    }
+}
